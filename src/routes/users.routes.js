5bafdf6732063aff28fc61fdd6f88eb8a1022573
GomidesTs <gomidestulio5@gmail.com>
2022-09-15 18:32:19 -0300

    feat: user update route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,8 +5,7 @@ const UsersController = require('../controllers/UsersController')
 const usersRoutes = Router()
 const userController = new UsersController()
 
-
-
-usersRoutes.post('/',userController.create)
+usersRoutes.post('/', userController.create)
+usersRoutes.put('/:id', userController.update)
 
 module.exports = usersRoutes
