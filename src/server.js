04083cb4e78be37831b5b2ea639e45ee29c70323
GomidesTs <gomidestulio5@gmail.com>
2022-09-15 18:48:29 -0300

    feat: server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,30 @@
+require('express-async-errors')
+
 const express = require('express')
+const AppError = require('./utils/AppError')
+const routes = require('./routes')
+const migrationsRun = require('./database/sqlite/migrations')
 const app = express()
 const PORT = 3000
+
+migrationsRun()
+
+app.use(express.json())
+app.use(routes)
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
